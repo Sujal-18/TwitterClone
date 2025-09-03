15017c503f480e8b937242ec498741ee8d4687b5
Sujal <sujalphuse@gmail.com>
2025-09-03 13:16:06 +0530

    whole push

---
+++ frontend/src/components/common/Posts.jsx
@@ -0,0 +1,77 @@
+import Post from "./Post";
+import PostSkeleton from "../skeletons/PostSkeleton";
+import { useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
+
+const Posts = ({ feedType, username, userId }) => {
+	const API_URL = import.meta.env.VITE_API_URL;
+
+	const getPostEndpoint = () => {
+		switch (feedType) {
+			case "forYou":
+				return `${API_URL}/posts/all`;
+			case "following":
+				return `${API_URL}/posts/following`;
+			case "posts":
+				return `${API_URL}/posts/user/${username}`;
+			case "likes":
+				return `${API_URL}/posts/likes/${userId}`;
+			default:
+				return `${API_URL}/posts/all`;
+		}
+	};
+
+	const POST_ENDPOINT = getPostEndpoint();
+
+	const {
+		data: posts,
+		isLoading,
+		refetch,
+		isRefetching,
+	} = useQuery({
+		queryKey: ["posts"],
+		queryFn: async () => {
+			try {
+				const res = await fetch(POST_ENDPOINT, {
+  				credentials: "include",
+			});
+				const data = await res.json();
+
+				if (!res.ok) {
+					throw new Error(data.error || "Something went wrong");
+				}
+
+				return data;
+			} catch (error) {
+				throw new Error(error);
+			}
+		},
+	});
+
+	useEffect(() => {
+		refetch();
+	}, [feedType, refetch, username]);
+
+	return (
+		<>
+			{(isLoading || isRefetching) && (
+				<div className='flex flex-col justify-center'>
+					<PostSkeleton />
+					<PostSkeleton />
+					<PostSkeleton />
+				</div>
+			)}
+			{!isLoading && !isRefetching && posts?.length === 0 && (
+				<p className='text-center my-4'>No posts in this tab. Switch 👻</p>
+			)}
+			{!isLoading && !isRefetching && posts && (
+				<div>
+					{posts.map((post) => (
+						<Post key={post._id} post={post} />
+					))}
+				</div>
+			)}
+		</>
+	);
+};
+export default Posts;
---
+++ frontend/src/hooks/useFollow.jsx
@@ -0,0 +1,39 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+
+const useFollow = () => {
+	const API_URL = import.meta.env.VITE_API_URL;
+	const queryClient = useQueryClient();
+
+	const { mutate: follow, isPending } = useMutation({
+		mutationFn: async (userId) => {
+			try {
+				const res = await fetch(`${API_URL}/users/follow/${userId}`, {
+					method: "POST",
+					credentials: "include"
+				});
+
+				const data = await res.json();
+				if (!res.ok) {
+					throw new Error(data.error || "Something went wrong!");
+				}
+				return;
+			} catch (error) {
+				throw new Error(error.message);
+			}
+		},
+		onSuccess: () => {
+			Promise.all([
+				queryClient.invalidateQueries({ queryKey: ["suggestedUsers"] }),
+				queryClient.invalidateQueries({ queryKey: ["authUser"] }),
+			]);
+		},
+		onError: (error) => {
+			toast.error(error.message);
+		},
+	});
+
+	return { follow, isPending };
+};
+
+export default useFollow;
